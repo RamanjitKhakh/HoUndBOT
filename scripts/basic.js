952c1a780a80dfb7b45e2e2016dd589d53dad7b6
raman <ramanjit456@yahoo.co.in>
2016-01-23 09:15:21 -0500

    basic houndify request works

--- scripts/basic.js
+++ scripts/basic.js
@@ -3,9 +3,39 @@
 //
 //  Author:
 //  Ramanjit Khakh person@temple.edu
+
+var Houndify = require('houndify').Houndify
+
+
+
 module.exports = function(robot){
+
+	var client = new Houndify({
+	  auth: {
+	    clientId: process.env.USER_ID,
+	    clientKey: process.env.USER_KEY,
+	    userId: 'houndify_try_api_user',
+	  },
+	  // Global RequestInfo, sent with every request
+	  requestInfo: {
+	    ClientID: 'Some application name'
+	  }
+	});
+
+
+
 	robot.respond(/message active/i, function(msg){
 		console.log("wakafloca flame");		
-
+		var input = 'What time is it in Tokyo?';
+		var querySpecificRequestInfo = {
+		  City: 'new york'
+		};
+		 
+		client.query(input, querySpecificRequestInfo, function(err, res) {
+					console.log("respond payload")
+					console.log(res);
+				
+		});
 	});
+
 }
