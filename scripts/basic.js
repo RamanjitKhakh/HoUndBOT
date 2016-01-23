50d974d907c31019e6e95667d7853b533e78e93c
raman <ramanjit456@yahoo.co.in>
2016-01-23 00:13:53 -0500

    basic script demo added

---
+++ scripts/basic.js
@@ -0,0 +1,11 @@
+// Description
+//  who to pm
+//
+//  Author:
+//  Ramanjit Khakh person@temple.edu
+module.exports = function(robot){
+	robot.respond(/message active/i, function(msg){
+		console.log("wakafloca flame");		
+
+	});
+}
